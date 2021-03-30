008825e6f0a48e7cff4f9eec285e6d88c65620f5
chinmaychahar <chinmay.cc.06@gmail.com>
2021-03-31 00:50:04 +0530

    Final changes made

--- app/src/main/java/com/example/benevole/UserProfile.java
+++ app/src/main/java/com/example/benevole/UserProfile.java
@@ -2,15 +2,30 @@ package com.example.benevole;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class UserProfile extends AppCompatActivity {
+
+    public Button save_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_profile);
+
+        save_btn = findViewById(R.id.save_profile);
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(UserProfile.this, "Profile Saved Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
