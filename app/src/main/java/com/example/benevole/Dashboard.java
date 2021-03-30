456b163fa6b7a2123c4f311ec49d1c91cae5efe1
chinmaychahar <chinmay.cc.06@gmail.com>
2021-03-30 23:58:56 +0530

    Final commits

--- app/src/main/java/com/example/benevole/Dashboard.java
+++ app/src/main/java/com/example/benevole/Dashboard.java
@@ -12,7 +12,7 @@ public class Dashboard extends AppCompatActivity {
 
     public Button button1;
     public Button button2;
-    public Button profile_button;
+    public Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +22,7 @@ public class Dashboard extends AppCompatActivity {
 
         button1=findViewById(R.id.opp_button);
         button2=findViewById(R.id.vol_button);
-        profile_button=findViewById(R.id.prf_btn);
+        button3=findViewById(R.id.user_btn);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,11 +38,12 @@ public class Dashboard extends AppCompatActivity {
             }
         });
 
-        profile_button.setOnClickListener(new View.OnClickListener() {
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Dashboard.this, UserProfile.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/example/benevole/UserProfile.java
+++ app/src/main/java/com/example/benevole/UserProfile.java
@@ -1,65 +1,16 @@
 package com.example.benevole;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.view.WindowManager;
 
 public class UserProfile extends AppCompatActivity {
-
-    private FirebaseUser user;
-    private DatabaseReference reference;
-
-    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_profile);
-
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("users");
-        userID = user.getUid();
-
-        final TextView usernameTextView = (TextView) findViewById(R.id.username);
-        final TextView emailTextView = (TextView) findViewById(R.id.email);
-        final TextView phoneTextView = (TextView) findViewById(R.id.phone);
-        final TextView passwordTextView = (TextView) findViewById(R.id.password);
-
-
-        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User userProfile = snapshot.getValue(User.class);
-
-                if(userProfile != null){
-                    String username = userProfile.username;
-                    String email = userProfile.email;
-                    String phone = userProfile.phoneNo;
-                    String password = userProfile.password;
-
-                    usernameTextView.setText(username);
-                    emailTextView.setText(email);
-                    phoneTextView.setText(username);
-                    passwordTextView.setText(password);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(UserProfile.this, "Something Wrong Happened", Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 }
